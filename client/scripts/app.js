8dd14f338528e11af8aa75c13bcda28c1aafe2db
bcjl <briancjliu@gmail.com>
2015-04-06 12:26:32 -0700

    Working on GET

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,37 @@
 // YOUR CODE HERE:
+var message = {
+  'username': 'foobar',
+  'text': 'foobar',
+  'roomname': 'foobar'
+};
 
+$.ajax({
+  // always use this url
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  type: 'POST',
+  data: JSON.stringify(message),
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('chatterbox: Message sent');
+  },
+  error: function (data) {
+    // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message');
+  }
+});
+
+
+$.ajax({
+  // always use this url
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  type: 'GET',
+  data: JSON.stringify(message),
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('chatterbox: GET success');
+  },
+  error: function (data) {
+    // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: GET Failed');
+  }
+});
