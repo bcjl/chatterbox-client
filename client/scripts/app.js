57cb2bd64cf88933bee4f47a6c3dac97d0eaec77
bcjl <briancjliu@gmail.com>
2015-04-06 19:54:24 -0700

    Refactored to pseudoclassical, passed some specs

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,18 +1,83 @@
+// Reference: https://github.com/janl/mustache.js/blob/master/mustache.js#L82
+
 // YOUR CODE HERE:
-var entityMap = {
-  "&": "&amp;",
-  "<": "&lt;",
-  ">": "&gt;",
-  '"': '&quot;',
-  "'": '&#39;',
-  "/": '&#x2F;'
+
+var App = function(){
+  this.escapeFilter = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': '&quot;',
+    "'": '&#39;',
+    "/": '&#x2F;'
+  };
+
+  this.queryFilter = {
+    limit: 10,
+    order: "-updatedAt"
+    // notEqualTo: "username:ScreamBot"
+  };
 };
 
-function escapeHtml(string) {
-  return String(string).replace(/[&<>"'\/]/g, function (s) {
-    return entityMap[s];
+
+App.prototype.init = function(){
+  var that = this;
+  that.fetch(this.queryFilter); //Why needs THAT?!
+};
+
+App.prototype.send = function(message){
+  $.ajax({
+    // always use this url
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
   });
-}
+};
+
+App.prototype.fetch = function(filter){
+
+  var that = this;
+  $.ajax({
+    // always use this url
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'GET',
+    data: filter,
+    contentType: 'application/json',
+
+    success: function(data){
+      console.log(data.results.length);
+      console.log(data.results);
+      var results = data.results;
+      results.forEach(function(msg){
+        var username = that.escapeHtml(msg.username);
+        var text = that.escapeHtml(msg.text);
+        var display = username + " : " + text;
+        var tag = '<p>' + display + '</p>';
+        $('#main').append(tag);
+      });
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: GET Failed');
+    }
+  });
+};
+
+
+App.prototype.escapeHtml = function(string) {
+  var that = this;
+  return String(string).replace(/[&<>"'\/]/g, function (s) {
+    return that.escapeFilter[s];
+  });
+};
 
 
 var message = {
@@ -21,52 +86,12 @@ var message = {
   'roomname': 'foobar'
 };
 
-$.ajax({
-  // always use this url
-  url: 'https://api.parse.com/1/classes/chatterbox',
-  type: 'POST',
-  data: JSON.stringify(message),
-  contentType: 'application/json',
-  success: function (data) {
-    console.log('chatterbox: Message sent');
-  },
-  error: function (data) {
-    // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message');
-  }
-});
+
+var app = new App();
+
+// app.init();
+
+// app.fetch(app.queryFilter);
 
 
-$.ajax({
-  // always use this url
-  url: 'https://api.parse.com/1/classes/chatterbox',
-  type: 'GET',
-  data: {limit: 1000, skip: 1000},
-  contentType: 'application/json',
-
-  success: function(data){
-    console.log(data.results.length);
-    // console.table(data.results);
-    var results = data.results;
-    results.forEach(function(msg){
-      var username = escapeHtml(msg.username);
-      var text = escapeHtml(msg.text);
-      var display = username + " : " + text;
-      var tag = '<p>' + display + '</p>';
-      $('#main').append(tag);
-    });
-  },
-  error: function (data) {
-    // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: GET Failed');
-  }
-});
-
-// var updatePage = function(data){
-//   return function(data){
-//     console.log('chatterbox: GET success');
-//     var res = data;
-//     console.log('inside',data);
-//   };
-// };
 
